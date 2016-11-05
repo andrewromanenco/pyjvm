cb8994d5740119c1f6d923d8c88206b9b004440c
Andrew Romanenco <andrew@romanenco.com>
2016-11-05 01:27:05 -0400

    Refactor attributes to separate module

---
+++ pyjvm/classfile/attributes.py
@@ -0,0 +1,24 @@
+"""Field and method attributes."""
+
+from collections import namedtuple
+
+Attribute = namedtuple(
+    'Attribute',
+    ['attribute_name_index', 'attribute_length', 'info']
+    )
+
+def read_attributes(reader):
+    """Read attributes from a stream."""
+    attr_count = reader.get_u2()
+    attributes = []
+    while attr_count > 0:
+        attribute_name_index = reader.get_u2()
+        attribute_length = reader.get_u2()
+        info = reader.get_uv(attribute_length)
+        attributes.append(Attribute(
+            attribute_name_index,
+            attribute_length,
+            info
+            ))
+        attr_count -= 1
+    return attributes
--- pyjvm/classfile/fields.py
+++ pyjvm/classfile/fields.py
@@ -1,6 +1,7 @@
 """Class fields."""
 
 from collections import namedtuple
+from pyjvm.classfile.attributes import read_attributes
 
 Field = namedtuple(
     'Field',
@@ -29,19 +30,3 @@ def read_fields(reader):
             ))
         fields_count -= 1
     return fields
-
-def read_attributes(reader):
-    """Read attributes from a stream."""
-    attr_count = reader.get_u2()
-    attributes = []
-    while attr_count > 0:
-        attribute_name_index = reader.get_u2()
-        attribute_length = reader.get_u2()
-        info = reader.get_uv(attribute_length)
-        attributes.append(Attribute(
-            attribute_name_index,
-            attribute_length,
-            info
-            ))
-        attr_count -= 1
-    return attributes
