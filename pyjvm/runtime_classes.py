1c74b86bd98010494eb26eb966146ed5d57c7a55
Andrew Romanenco <andrew@romanenco.com>
2016-11-19 17:54:50 -0500

    Add caching for class names

--- pyjvm/runtime_classes.py
+++ pyjvm/runtime_classes.py
@@ -8,7 +8,11 @@ class RuntimeClass:
     def __init__(self, java_class):
         '''Init with a java class.'''
         self.__java_class = java_class
+        self.__name = None
 
     def get_name(self):
         '''Return java binary class name. This is a string.'''
-        return resolve_class_name(self.__java_class)
+        if self.__name is not None:
+            return self.__name
+        self.__name = resolve_class_name(self.__java_class)
+        return self.__name
