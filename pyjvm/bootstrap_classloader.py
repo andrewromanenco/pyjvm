772ee705ac5aa97efa04bed0bd887ef37ca3c8e9
Andrew Romanenco <andrew@romanenco.com>
2016-11-19 18:08:03 -0500

    Add bootstrap class loader

---
+++ pyjvm/bootstrap_classloader.py
@@ -0,0 +1,33 @@
+'''VM class loaders.'''
+
+from pyjvm.class_parser import ClassParser
+from pyjvm.runtime_classes import RuntimeClass
+
+class BootstrapClassloader():
+    '''VM bootstrap class loader.'''
+
+    def __init__(self, classpath):
+        '''Init with a classpath.'''
+        self.classpath = classpath
+
+    def load_class(self, class_name):
+        '''Get runtime class for a given binary class name.'''
+        bytecode = self.classpath.bytes(class_name)
+        if bytecode is None:
+            raise Exception('No such class ' + class_name)
+        java_class = ClassParser().parse(_BytesToStream(bytecode))
+        return RuntimeClass(java_class)
+
+class _BytesToStream:
+    '''Temp class. To be refactored out.'''
+
+    def __init__(self, data_bytes):
+        '''Init with bytes.'''
+        self.__data = data_bytes
+        self.__pointer = 0
+
+    def read(self, num_of_bytes):
+        '''Read bytes from pointer and move the pointer.'''
+        result = self.__data[self.__pointer:self.__pointer + num_of_bytes]
+        self.__pointer += num_of_bytes
+        return result
---
+++ tests/test_bootstrap_classloader.py
@@ -0,0 +1,18 @@
+import unittest
+
+from pyjvm.bootstrap_classloader import BootstrapClassloader
+from pyjvm.classpath import ClassPath
+
+class TestBootstrapClassloader(unittest.TestCase):
+
+    def test_load_class(self):
+        classpath = ClassPath()
+        classpath.add('tests/res/sample.jar')
+        testee = BootstrapClassloader(classpath)
+        runtime_class = testee.load_class('some/pkg/JarredClass')
+        self.assertIsNotNone(runtime_class)
+        self.assertEqual('some/pkg/JarredClass', runtime_class.get_name())
+
+
+if __name__ == '__main__':
+    unittest.main()
